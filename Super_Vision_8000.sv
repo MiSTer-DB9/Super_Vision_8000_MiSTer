0227aaeec4df5909c431640fef95d7482aa2862f
Timothy Redaelli <timothy.redaelli@gmail.com>
2026-06-09 15:38:41 +0200

    DB9: consume programmable remap matrix at joystick merge

--- Super_Vision_8000.sv
+++ Super_Vision_8000.sv
@@ -296,8 +296,8 @@ wire [127:0] status;
 wire  [10:0] ps2_key;
 // [MiSTer-DB9 BEGIN] - DB9/SNAC8 support: USB-side joystick wires renamed + joydb mux
 wire  [31:0] joy0_USB, joy1_USB;
-wire  [31:0] joy0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : {16'b0, joydb_1}) : joy0_USB;
-wire  [31:0] joy1 = joydb_2ena ? (OSD_STATUS ? 32'b0 : {16'b0, joydb_2}) : (joydb_1ena ? joy0_USB : joy1_USB);
+wire  [31:0] joy0 = joydb_1ena ? (OSD_STATUS ? 32'b0 : joydb_1_mapped[15:0]) : joy0_USB;
+wire  [31:0] joy1 = joydb_2ena ? (OSD_STATUS ? 32'b0 : joydb_2_mapped[15:0]) : (joydb_1ena ? joy0_USB : joy1_USB);
 // [MiSTer-DB9 END]
 wire         ioctl_download;
 wire   [7:0] ioctl_index;
